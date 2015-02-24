59fc977ec2278a66bf96904b35c679be8ff6ce1b
Anatoly Chernow <f0ck1ng.namespace@gmail.com>
2015-02-24 16:19:55 +0200

    These curly braces, indeed, are required

--- lib/include_constants/core_ext/module.rb
+++ lib/include_constants/core_ext/module.rb
@@ -3,7 +3,7 @@ class Module
     constants = from.constants if constants.empty?
     constants.each do |constant|
       unless from.constants.include? constant
-        raise "Cannot include the constant #constant from #from, because #from doesn't have one with such a name."
+        raise "Cannot include the constant #{constant} from #{from}, because #{from} doesn't have one with such a name."
       end
       const_set constant, (const_get "#{from}::#{constant}")
     end
